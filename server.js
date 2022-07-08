02ff68961875d9b295b2f717abbf74a17cadef2e
Kayden Lee <kayden@programmer.net>
2022-07-08 14:59:29 +0800

    update

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const path = require('path');
+const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/ping', function (req, res) {
+ return res.send('pong');
+});
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+console.info("Server started")
+app.listen(process.env.PORT || 8080);
